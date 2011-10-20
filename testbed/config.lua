df137a490e1e8c87cf1d3e04f3e8fa71ebf7b424
Jonathan Beebe <jonathan@anscamobile.com>
2011-10-20 15:33:16 -0700

    * Reworked widget library to fix bugs and be more efficient and flexible overall. * Reduced code from 6468 lines to only 2403 total lines.

--- testbed/config.lua
+++ testbed/config.lua
@@ -1,62 +1,12 @@
-local platform = system.getInfo( "platformName" )
-local isAndroid = false
-
-if platform == "Mac OS X" then
-	local model = system.getInfo( "model" )
-	
-	if model == "Nexus One" or model == "myTouch" or model == "Droid" or model == "Galaxy Tab" then
-		isAndroid = true
-	end
-end
-
-if not isAndroid then
-	application = 
-	{
-		content = 
-		{ 
-			width = 320,
-			height = 480,
-			scale = "letterbox",
-			fps = 30,
-			
-			imageSuffix = {
-				["@2x"] = 2,
-			}
-		}
+application = {
+	content = {
+		width = 320,
+		height = 480,
+		scale = "letterbox",
+		fps = 60,
+		
+		imageSuffix = {
+		    ["@2x"] = 2,
+		},
 	}
-else
-	application = 
-	{
-		content = 
-		{ 
-			width = 320,
-			height = 480,
-			scale = "letterbox",
-			fps = 30,
-			
-			imageSuffix = {
-				["@2x"] = 2,
-			}
-		}
-	}
-end
-
-local sysModel = system.getInfo("model")
-
-if sysModel == "iPad" then
-
-	application = 
-	{
-		content = 
-		{ 
-			width = 768,
-			height = 1024,
-			scale = "letterbox",
-			fps = 30,
-			
-			imageSuffix = {
-				["@2x"] = 2,
-			}
-		}
-	}
-end
+}
--- testbed/main.lua
+++ testbed/main.lua
@@ -1,92 +1,177 @@
---*********************************************************************************************
---
--- ====================================================================
--- Corona SDK "Widget" Sample Code
--- ====================================================================
---
--- File: main.lua
---
--- Version 1.3
---
--- Copyright (C) 2011 ANSCA Inc. All Rights Reserved.
---
--- Permission is hereby granted, free of charge, to any person obtaining a copy of 
--- this software and associated documentation files (the "Software"), to deal in the 
--- Software without restriction, including without limitation the rights to use, copy, 
--- modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
--- and to permit persons to whom the Software is furnished to do so, subject to the 
--- following conditions:
--- 
--- The above copyright notice and this permission notice shall be included in all copies 
--- or substantial portions of the Software.
--- 
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
--- INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
--- PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
--- FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
--- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
--- DEALINGS IN THE SOFTWARE.
---
--- Published changes made to this software and associated documentation and module files (the
--- "Software") may be used and distributed by ANSCA, Inc. without notification. Modifications
--- made to this software and associated documentation and module files may or may not become
--- part of an official software release. All modifications made to the software will be
--- licensed under these same terms and conditions.
---
---*********************************************************************************************
+display.setStatusBar( display.DefaultStatusBar )
 
-display.setStatusBar( display.DarkStatusBar )
+local widget = require( "widgetlib" )
+widget.setTheme( "theme_ios" )
 
-local widget = require "widgetnew"
+local options = {}
+options.left = 0
+options.top = display.statusBarHeight
+options.topPadding = 0
+options.bottomPadding = 0
+options.height = 410
+options.maskFile = "assets/mask-410.png"
 
--- Skin options: ios, red, mono
---widget.setSkin( "mono" )
+local list = widget.new( "tableView", options )
 
--- provide access to widgets to external module
-skinSetting = widget.skinSetting
-newEmbossedText = widget.newEmbossedText
-newButton = widget.newButton
-newSegmentedControl = widget.newSegmentedControl
-newSlider = widget.newSlider
-newToolbar = widget.newToolbar
-newScrollView = widget.newScrollView
-newTableView = widget.newTableView
-newPickerWheel = widget.newPickerWheel
-newRoundedRectButton = widget.newRoundedRectButton
+local startN = 1
 
--- vars
-listBoxStart = display.statusBarHeight + 44	--> 44 is the default height of toolbar
-local platformName = system.getInfo( "platformName" )
-local modelName = system.getInfo( "model" )
-local isAndroid = platformName == "Android"
-local isAndroidSim = platformName == "Mac OS X" and modelName ~= "iPhone" and modelName ~= "iPhone4" and modelName ~= "iPad" and modelName ~= "iPhone Simulator"
-local isiOS = platformName == "iPhone" or platformName == "iOS" or modelName == "iPod touch" or modelName == "iPhone" or modelName == "iPhone4" or modelName == "iPad"
-local isiOSsim = platformName == "Mac OS X" and modelName == "iPhone" or modelName == "iPhone4" or modelName == "iPad" or modelName == "iPhone Simulator"
+------------------------------
 
--- forward references
-local apiList, appToolbar
-
-if isiOS or isiOSsim then
+local function onRowRender( event )
+	local row = event.target
+	local i = event.index
+	local rowGroup = event.view
+	local rowX = 15
+	if row.isCategory then rowX=12 end
+	local fontSize = 18
 	
-	-- For iOS, load a different module depending on if user's device is
-	-- an iPhone/iPod touch or an iPad.
-	
-	widget.setSkin( "iOS" )
-	skinSetting = widget.skinSetting
-	
-	if modelName == "iPad" then
-		require( "ipad" ).loadMod()
-	
+	local rowString = "Row Item #" .. i
+	local text = display.newRetinaText( rowString, rowX, 0, "Helvetica-Bold", fontSize )
+	text:setReferencePoint( display.CenterLeftReferencePoint )
+	text.y = row.height * 0.5-1
+	if row.isCategory then
+		text:setTextColor( 255, 255, 255, 255 )
 	else
-		require( "iphone" ).loadMod()
+		text:setTextColor( 0, 0, 0, 255 )
 	end
-
-elseif isAndroid or isAndroidSim then
 	
-	-- Widgets should now work fine on Android (beta). It's recommended you use the "red" or "mono" theme.
-	
-	widget.setSkin( "red" )
-	skinSetting = widget.skinSetting
-	
-	require( "iphone" ).loadMod()
+	rowGroup:insert( text )
 end
+
+------------------------------
+
+local function onRowEvent( event )
+	local row = event.target
+	local rowGroup = event.view
+	
+	if event.phase == "press" then
+		if not row.isCategory then rowGroup.alpha = 0.5; end
+
+	elseif event.phase == "release" then
+		
+		if not row.isCategory then
+			row.reRender = true
+			print( "You touched " .. event.index .. "." )
+		end
+	end
+	
+	return true
+end
+
+------------------------------
+
+local function addTwentyFive()
+	for i=startN,startN+99 do
+		
+		local rowHeight, isCategory, rowColor, lineColor
+		if i == 25 then rowHeight = 24; isCategory = true; rowColor={ 70, 70, 130, 255 }; lineColor={0,0,0,255}; end
+		if i == 45 then rowHeight = 24; isCategory = true; rowColor={ 70, 70, 130, 255 }; lineColor={0,0,0,255}; end
+
+		list:insertRow{
+			onEvent=onRowEvent,
+			onRender=onRowRender,
+			height=rowHeight,
+			isCategory=isCategory,
+			rowColor=rowColor,
+			lineColor=lineColor
+		}
+	end
+	
+	startN = startN+49
+end
+
+addTwentyFive()
+
+local onTabPress = function( event )
+	print( "You pressed a tab button: " .. event.target.id )
+end
+
+-- buttons
+local buttonsTable = {
+	{ label="First Tab", up="assets/coronaIcon.png", down="assets/coronaIcon-down.png", width=32, height=32, onPress=onTabPress, selected=true },
+	{ label="Second", up="assets/coronaIcon.png", down="assets/coronaIcon-down.png", width=32, height=32, onPress=onTabPress },
+	--{ label="Third", up="assets/coronaIcon.png", down="assets/coronaIcon-down.png", width=32, height=32, onPress=onTabPress },
+	--{ label="Fourth", up="assets/coronaIcon.png", down="assets/coronaIcon-down.png", width=32, height=32, onPress=onTabPress },
+}
+
+-- create tab bar
+local tabBar = widget.new( "tabBar", { top=480-50 }, buttonsTable )
+
+--tabBar:removeSelf()
+--list:removeSelf()
+
+--list:scrollToIndex( 2 )
+
+timer.performWithDelay( 2000, function()
+	--list:deleteRow( 100 )
+	--list:scrollToIndex( 50 )
+end, 1 )
+
+local columnData = {
+	{ "One", "Two", "Three", "Four", "Five" },
+	--{ "One", "Two", "Three", "Four", "Five" },
+	{ "AM", "PM" }
+}
+
+columnData[1].width = 168
+columnData[1].startIndex = 4
+
+--[[
+local picker = widget.new( "pickerWheel", {
+	id="pickerWheel",
+	font="Helvetica-Bold",
+	top=258-50,
+	columns=columnData,
+} )
+--]]
+
+local function getColumnData()
+	local columnData = picker:getValues()
+	
+	for i=1,#columnData do
+		print( columnData[i].value, columnData[i].index )
+	end
+end
+
+timer.performWithDelay( 4000, function()
+	--picker:removeSelf()
+end, 1 )
+
+--local slider = widget.new( "slider", { x=100, y=200, width=100 } )
+
+--timer.performWithDelay( 2000, function() list:removeSelf(); list = nil; end, 1 )
+
+--local text1 = display.newRetinaText( "Hello World! I'm hoping this will finally work!", 100, 200, 200, 200, "Helvetica-Bold", 18 )
+--local text2 = display.newText( "Hello World! I'm hoping this will finally work!", 100, 200, 200, 200, "Helvetica-Bold", 18 )
+
+--print( text1.width )
+
+--timer.performWithDelay(3000, function() list:scrollToY( -882, 0 ); end, 1 )
+
+--timer.performWithDelay( 5000, addTwentyFive, 5 )
+
+--timer.performWithDelay( 3000, function() list:removeSelf(); list = nil; end, 1 )
+
+--[[
+local black1 = display.newRect( 50, 0, 150, 40 )
+black1:setFillColor( 50, 0, 25, 255 )
+
+local black2 = display.newRect( 50, 42, 150, 40 )
+black2:setFillColor( 50, 0, 25, 255 )
+
+local black3 = display.newRect( 50, 84, 150, 40 )
+black3:setFillColor( 50, 0, 25, 255 )
+
+list._view:insert( black1 )
+list._view:insert( black2 )
+list._view:insert( black3 )
+
+
+black2:setReferencePoint( display.TopLeftReferencePoint )
+black2.y = black2.y + 100
+
+
+--]]
+
+
+--Runtime:addEventListener( "enterFrame", function() print( list._view.content.rows[1].top ); end )
