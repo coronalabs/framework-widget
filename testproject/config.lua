c5296bb51127a6576ca25cab06a06034a03e6344
Jonathan Beebe <jonathan@anscamobile.com>
2012-01-05 12:09:00 -0800

    * Updated to widget 0.5b changes. Replaced test project and added updated iOS theme files.

--- testproject/config.lua
+++ testproject/config.lua
@@ -1,12 +1,14 @@
-application = {
-	content = {
-		--width = 320,
-		--height = 480,
+application = 
+{
+	content = 
+	{ 
+		width = 320,
+		height = 480,
 		scale = "letterbox",
 		fps = 60,
 		
 		imageSuffix = {
-		    ["@2x"] = 2,
-		},
+			["@2x"] = 2,
+		}
 	}
 }
--- testproject/theme_ios.lua
+++ testproject/theme_ios.lua
@@ -19,12 +19,12 @@ local assetDir = "widget_ios/"
 -- example:
 -- local button = widget.newButton{ style="blue1Small" }
 --
--- NOTE: using a "style" is not required.
+-- NOTE: using a "style" is not required (default options will be used).
 --
 -----------------------------------------------------------------------------------------
 
 themeTable.button = {
-	-- if no style is specified, will use button default:
+	-- if no style is specified, will use default:
 	default = assetDir .. "button/default.png",
 	over = assetDir .. "button/over.png",
 	width = 278, height = 46,
@@ -33,11 +33,13 @@ themeTable.button = {
 	labelColor = { default={0}, over={255} },
 	emboss = true,
 	
+	-- button styles
+	
 	blue1Small = {
 		default = assetDir .. "button/blue1Small/default.png",
 		over = assetDir .. "button/blue1Small/over.png",
 		width = 60, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -47,7 +49,7 @@ themeTable.button = {
 		default = assetDir .. "button/blue1Large/default.png",
 		over = assetDir .. "button/blue1Large/over.png",
 		width = 90, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -57,7 +59,7 @@ themeTable.button = {
 		default = assetDir .. "button/blue2Small/default.png",
 		over = assetDir .. "button/blue2Small/over.png",
 		width = 60, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -67,7 +69,7 @@ themeTable.button = {
 		default = assetDir .. "button/blue2Large/default.png",
 		over = assetDir .. "button/blue2Large/over.png",
 		width = 90, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -77,7 +79,7 @@ themeTable.button = {
 		default = assetDir .. "button/blackSmall/default.png",
 		over = assetDir .. "button/blackSmall/over.png",
 		width = 60, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -87,7 +89,7 @@ themeTable.button = {
 		default = assetDir .. "button/blackLarge/default.png",
 		over = assetDir .. "button/blackLarge/over.png",
 		width = 90, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -97,7 +99,7 @@ themeTable.button = {
 		default = assetDir .. "button/redSmall/default.png",
 		over = assetDir .. "button/redSmall/over.png",
 		width = 60, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -107,7 +109,7 @@ themeTable.button = {
 		default = assetDir .. "button/redLarge/default.png",
 		over = assetDir .. "button/redLarge/over.png",
 		width = 90, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -117,7 +119,7 @@ themeTable.button = {
 		default = assetDir .. "button/backSmall/default.png",
 		over = assetDir .. "button/backSmall/over.png",
 		width = 60, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -127,7 +129,7 @@ themeTable.button = {
 		default = assetDir .. "button/backLarge/default.png",
 		over = assetDir .. "button/backLarge/over.png",
 		width = 90, height = 30,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 12,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -137,7 +139,7 @@ themeTable.button = {
 		default = assetDir .. "button/sheetGreen/default.png",
 		over = assetDir .. "button/sheetGreen/over.png",
 		width = 278, height = 46,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 20,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -147,7 +149,7 @@ themeTable.button = {
 		default = assetDir .. "button/sheetRed/default.png",
 		over = assetDir .. "button/sheetRed/over.png",
 		width = 278, height = 46,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 20,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -157,7 +159,7 @@ themeTable.button = {
 		default = assetDir .. "button/sheetBlack/default.png",
 		over = assetDir .. "button/sheetBlack/over.png",
 		width = 278, height = 46,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 20,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -167,7 +169,7 @@ themeTable.button = {
 		default = assetDir .. "button/sheetYellow/default.png",
 		over = assetDir .. "button/sheetYellow/over.png",
 		width = 278, height = 46,
-		font = "Helvetica-Bold",
+		font = "HelveticaNeue-Bold",
 		fontSize = 20,
 		labelColor = { default={255}, over={255} },
 		emboss = true,
@@ -182,12 +184,24 @@ themeTable.button = {
 -----------------------------------------------------------------------------------------
 
 themeTable.slider = {
-	width = 220,
-	leftImage = { assetDir .. "slider/slider_left.png", 32, 28 },
-	rightImage = { assetDir .. "slider/slider_right.png", 32, 28 },
-	maskImage = { assetDir .. "slider/slider_mask.png", 920, 64 },
-	fillImage = { assetDir .. "slider/slider_fill.png", 800, 32 },
-	handleImage = { assetDir .. "slider/slider_handle.png", 24, 28 }
+	-- default style
+	width = 220, height = 10,
+	background = assetDir .. "slider/sliderBg.png",
+	fillImage = assetDir .. "slider/sliderFill.png",
+	fillWidth = 2, leftWidth = 16,
+	handle = assetDir .. "slider/handle.png",
+	handleWidth = 32, handleHeight = 32,
+	
+	-- slider styles
+	
+	small120 = {
+		width = 120, 10,
+		background = assetDir .. "slider/small120/sliderBg.png",
+		fillImage = assetDir .. "slider/sliderFill.png",
+		fillWidth = 2, leftWidth = 16,
+		handle = assetDir .. "slider/handle.png",
+		handleWidth = 32, handleHeight = 32
+	}
 }
 
 -----------------------------------------------------------------------------------------
@@ -198,13 +212,15 @@ themeTable.slider = {
 
 themeTable.pickerWheel = {
 	width = 296,
-	maskFile=assetDir .. "pickerWheel/pickermask.png",
-	glassFile=assetDir .. "pickerWheel/pickerglass.png",
-	glassWidth=320, glassHeight=222,
-	background=assetDir .. "pickerWheel/bg.png",
-	backgroundWidth=1, backgroundHeight=222,
+	maskFile=assetDir .. "pickerWheel/wheelmask.png",
+	overlayImage=assetDir .. "pickerWheel/overlay.png",
+	overlayWidth=320, overlayHeight=222,
+	bgImage=assetDir .. "pickerWheel/bg.png",
+	bgImageWidth=1, bgImageHeight=222,
 	separator=assetDir .. "pickerWheel/separator.png",
-	separatorWidth=8, separatorHeight=1
+	separatorWidth=8, separatorHeight=1,
+	font = "HelveticaNeue-Bold",
+	fontSize = 22
 }
 
 -----------------------------------------------------------------------------------------
