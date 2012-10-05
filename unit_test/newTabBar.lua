7cd4beda2abd4333059a455724c53ed69214603d
Danny <danny@anscamobile.com>
2012-10-05 23:34:42 +0100

    1) Removed duplicated widgets contained in the unit_test folder. We now require the widgets from the root directory for our unit tests. 2) Removed widgetnew dependency. We now just require the widget.lua file from the root directory for our unit tests. 3) Removed widget_constructor.lua. It's construction method has been moved to widget.lua 4) Cleaned up unit_test folder
    --HG--
    branch : Danny Branch WIP

--- unit_test/newTabBar.lua
+++ unit_test/newTabBar.lua
@@ -1,4 +1,25 @@
-local widget = require( "widgetnew" )
+-- Copyright (C) 2012 Corona Inc. All Rights Reserved.
+-- File: newTabBar unit test.
+
+-- Change the package.path and make it so we can require the "widget.lua" file from the root directory
+-------------------------------------------------------------------------------------------------
+local path = package.path
+
+-- get index of first semicolon
+local i = string.find( path, ';', 1, true )
+if ( i > 0 ) then
+	-- first path (before semicolon) is project dir
+	local projDir = string.sub( path, 1, i )
+
+	-- assume widget dir is parent to projDir
+	local widgetDir = string.gsub( projDir, '(.*)/([^/]?/\?\.lua)', '%1/../%2' )
+	package.path = widgetDir .. path
+end
+
+package.preload.widget = nil
+-------------------------------------------------------------------------------------------------
+
+local widget = require( "widget" )
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
 
