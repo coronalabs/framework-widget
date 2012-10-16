750b385595276663e9d4f1c4451542d928dfcf91
Danny <danny@anscamobile.com>
2012-10-16 23:35:36 +0100

    1) Fixes casenum: 16983 - Scroll view bug when scrolling fast and when just scrolling (Android)

--- unit_test/unitTest_newScrollView.lua
+++ unit_test/unitTest_newScrollView.lua
@@ -62,33 +62,40 @@ function scene:createScene( event )
 	local TEST_SCROLL_TO_LEFT = false
 	local TEST_SCROLL_TO_RIGHT = false
 	
-	--Forward reference to scrollView listener
+	--Function to listen for scrollView events
 	local function scrollListener( event )
-		print( "Event type:", event.type )
+		print( "ScrollView Event:", event.type )
 		return true
 	end
-	
-	--Create scrollView
+		
+	--Create a scrollView
 	local scrollView = widget.newScrollView{
-		top = 100,										--Test setting top position.
-		left = 10,										--Test setting left position.
-		width = 300, height = 350,						--Test setting width/height.
-		scrollWidth = 768, scrollHeight = 1024,			--Test setting scroll width/height.
-		maskFile = "assets/scrollViewMask-350.png",		--Test setting a mask.
-		--baseDir = system.DocumentsDirectory,			--Test base directory (Ensure maskFile specified above exists in the baseDir specified).
-		bgColor = { 255, 255, 255, 255 }, 				--Test setting a background color.
-		hideBackground = true, 			 			 	--Test hiding the background color.
-		scrollBarColor = { 255, 0, 128 }, 				--Test setting the scrollbar color. If this is ommited or not a table, the scrollbar falls back to it's default color.
-		hideScrollBar = true, 							--Test hiding the scrollbar. When set to true, scrollbar is shown when set to false or omitted.
-		listener = scrollListener						--Test setting a listener for the scrollView.
+		top = 0, left = 0,
+		width = 320, height = 480,
+		--bgColor = { 255, 0, 255, 255 }, 	--Un-Comment this to set the background color of the scrollView.
+		--scrollBarColor = { 255, 0, 128 }, --Un-Comment this to set the scrollBar to a custom color.
+		--hideScrollBar = true, 			--Un-Comment this to hide the scrollBar
+		listener = scrollListener
 	}
-	scrollView.isHitTestMasked = true
 	
-	-- insert image into scrollView widget
-	local bg = display.newImageRect( "assets/scrollimage.jpg", 768, 1024 )
-	bg:setReferencePoint( display.TopLeftReferencePoint )
-	bg.x, bg.y = 0, 0
-	scrollView:insert( bg )
+	
+	--Create a text object for the scrollViews title
+	local titleText = display.newText("Move Up to Scroll", 0, 0, native.systemFontBold, 16)
+	titleText:setTextColor(0, 0, 0)
+	titleText.x = display.contentCenterX
+	titleText.y = 48
+	scrollView:insert( titleText )
+	
+	--Create a large text string
+	local lotsOfText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet consectetur euismod. Phasellus non ipsum vel eros vestibulum consequat. Integer convallis quam id urna tristique eu viverra risus eleifend.\n\nAenean suscipit placerat venenatis. Pellentesque faucibus venenatis eleifend. Nam lorem felis, rhoncus vel rutrum quis, tincidunt in sapien. Proin eu elit tortor. Nam ut mauris pellentesque justo vulputate convallis eu vitae metus. Praesent mauris eros, hendrerit ac convallis vel, cursus quis sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, dui in vehicula dapibus, lorem nisi placerat turpis, quis gravida elit lectus eget nibh. Mauris molestie auctor facilisis.\n\nCurabitur lorem mi, molestie eget tincidunt quis, blandit a libero. Cras a lorem sed purus gravida rhoncus. Cras vel risus dolor, at accumsan nisi. Morbi sit amet sem purus, ut tempor mauris.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet consectetur euismod. Phasellus non ipsum vel eros vestibulum consequat. Integer convallis quam id urna tristique eu viverra risus eleifend.\n\nAenean suscipit placerat venenatis. Pellentesque faucibus venenatis eleifend. Nam lorem felis, rhoncus vel rutrum quis, tincidunt in sapien. Proin eu elit tortor. Nam ut mauris pellentesque justo vulputate convallis eu vitae metus. Praesent mauris eros, hendrerit ac convallis vel, cursus quis sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, dui in vehicula dapibus, lorem nisi placerat turpis, quis gravida elit lectus eget nibh. Mauris molestie auctor facilisis.\n\nCurabitur lorem mi, molestie eget tincidunt quis, blandit a libero. Cras a lorem sed purus gravida rhoncus. Cras vel risus dolor, at accumsan nisi. Morbi sit amet sem purus, ut tempor mauris. "
+	
+	--Create a text object containing the large text string and insert it into the scrollView
+	local lotsOfTextObject = display.newText( lotsOfText, 0, 0, 300, 0, "Helvetica", 14)
+	lotsOfTextObject:setTextColor( 0 ) 
+	lotsOfTextObject:setReferencePoint( display.TopCenterReferencePoint )
+	lotsOfTextObject.x = display.contentCenterX
+	lotsOfTextObject.y = titleText.y + titleText.contentHeight + 10
+	scrollView:insert(lotsOfTextObject)
 	
 	-- don't forget to insert objects into the scene group!
 	group:insert( scrollView )
