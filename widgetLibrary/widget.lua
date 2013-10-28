23a472ee854f810a33c5040692a1c71538b4b6f6
Alexandru Frangeti <alex@coronalabs.com>
2013-10-28 14:22:50 +0100

    Dedicated constructor for container-based widgets (tableview / scrollview )

--- widgetLibrary/widget.lua
+++ widgetLibrary/widget.lua
@@ -81,38 +81,41 @@ function widget._new( options )
 
 	local newWidget
 
-	if options.widgetType and ( options.widgetType == "scrollView" or options.widgetType == "tableView" ) and not isGraphicsV1 then
-		
-		newWidget = display.newContainer( display.contentWidth, display.contentHeight ) -- All Widget* objects are display groups, except scrollview and tableview
-		
-		newWidget.id = options.id or "widget*"
-		newWidget.baseDir = options.baseDir or system.ResourceDirectory
-		newWidget._isWidget = true
-		newWidget._widgetType = options.widgetType
-		newWidget._removeSelf = newWidget.removeSelf
-		newWidget.removeSelf = _removeSelf
-		newWidget._loseFocus = widget._loseFocus
-			
-	else
+	newWidget = display.newGroup() -- All Widget* objects are display groups, except scrollview and tableview
+	newWidget.x = options.left or 0
+	newWidget.y = options.top or 0
+	newWidget.id = options.id or "widget*"
+	newWidget.baseDir = options.baseDir or system.ResourceDirectory
+	newWidget._isWidget = true
+	newWidget._widgetType = options.widgetType
+	newWidget._removeSelf = newWidget.removeSelf
+	newWidget.removeSelf = _removeSelf
+	newWidget._loseFocus = widget._loseFocus
 
-		newWidget = display.newGroup() -- All Widget* objects are display groups, except scrollview and tableview
-		newWidget.x = options.left or 0
-		newWidget.y = options.top or 0
-		newWidget.id = options.id or "widget*"
-		newWidget.baseDir = options.baseDir or system.ResourceDirectory
-		newWidget._isWidget = true
-		newWidget._widgetType = options.widgetType
-		newWidget._removeSelf = newWidget.removeSelf
-		newWidget.removeSelf = _removeSelf
-		newWidget._loseFocus = widget._loseFocus
-	
-		if not isGraphicsV1 then
-			newWidget.anchorChildren = true
-		end
-	
+	if not isGraphicsV1 then
+		newWidget.anchorChildren = true
 	end
 	
 	return newWidget
+end
+
+-- G2.0 constructor for tableview / scrollview
+function widget._newContainer( options )
+
+	local newWidget
+
+	newWidget = display.newContainer( display.contentWidth, display.contentHeight ) -- All Widget* objects are display groups, except scrollview and tableview
+
+	newWidget.id = options.id or "widget*"
+	newWidget.baseDir = options.baseDir or system.ResourceDirectory
+	newWidget._isWidget = true
+	newWidget._widgetType = options.widgetType
+	newWidget._removeSelf = newWidget.removeSelf
+	newWidget.removeSelf = _removeSelf
+	newWidget._loseFocus = widget._loseFocus
+
+	return newWidget
+
 end
 
 local newWidgetV2 = function( newWidget, ... )
